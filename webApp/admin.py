ce07f2d427b84e262c70408c0ab81c0891ef325c
ChengTaChung <chungct0916@gmail.com>
2023-03-10 16:52:21 +0800

    Complete the addition of new features for each layer of the system, course, and unit. Add the function to upload HTML files and fix some bugs.

--- webApp/admin.py
+++ webApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import CourseCatalog, TeachCourse, TeachCourseUnit
 # Register your models here.
+admin.site.register(CourseCatalog)
+admin.site.register(TeachCourse)
+admin.site.register(TeachCourseUnit)
