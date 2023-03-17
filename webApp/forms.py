d4819fbeba2e2e483479ac8b09b070ac2b14830d
ChengTaChung <chungct0916@gmail.com>
2023-03-17 10:52:48 +0800

    fix unit display bug and file upload id

--- webApp/forms.py
+++ webApp/forms.py
@@ -2,7 +2,8 @@ from django import forms
 
 
 class EditCourseForm(forms.Form):
-    name = forms.CharField(label="課程名稱", widget=forms.TextInput(attrs={'class':'form-control'}), max_length=100)
-    descript = forms.CharField(label="內容",widget=forms.Textarea(attrs={'class':'form-control'}))
+    name = forms.CharField(label="課程名稱", widget=forms.TextInput(
+        attrs={'class': 'form-control'}), max_length=100)
+    descript = forms.CharField(
+        label="內容", widget=forms.Textarea(attrs={'class': 'form-control'}))
     file = forms.FileField(label="上傳檔案")
-
--- webApp/views.py
+++ webApp/views.py
@@ -1,15 +1,13 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import CourseCatalog, TeachCourseUnit, TeachCourse
 from .forms import EditCourseForm
 
 from django.conf import settings
-from uuid_upload_path import uuid # not used
+from uuid_upload_path import uuid  # not used
 import os
 import glob
-from hashlib import sha256 
-
-
+from hashlib import sha256
 
 
 # present web content
@@ -17,21 +15,20 @@ def index(request):
     return render(request, "index.html", locals())
 
 
-def showCatalog(request,courseName=""): 
+def showCatalog(request, courseName=""):
     allObject = CourseCatalog.objects.all()
     # mylocals = dict(locals()) # 用來檢查request參數
     return render(request, "showCatalog.html", locals())
 
 
-def showCourse(request, courseName=""): 
-    allObject= TeachCourse.objects.all()
+def showCourse(request, courseName=""):
+    allObject = TeachCourse.objects.all()
     return render(request, "showCourse.html", locals())
 
 
-def showUnit(request,status=None):
-    allObject = TeachCourseUnit.objects.all()# 沒有特異性
+def showUnit(request, status=None):
+    allObject = TeachCourseUnit.objects.all()  # 沒有特異性
     return render(request, "showUnit.html", locals())
-
 
 
 # 儲存至資料庫
@@ -39,26 +36,29 @@ def createCatalog(request):
     if request.method == 'POST':
         name = request.POST["name"].strip()
         descript = request.POST["descript"]
-        catalog = CourseCatalog.objects.create(CourseCatalogName=name, description=descript)
+        catalog = CourseCatalog.objects.create(
+            CourseCatalogName=name, description=descript)
         allNameOfCatalog = CourseCatalog.objects.all()
         catalog.save()
-        
+
         # check whether instance is created??
         # if catalog: # 成功建立實體
         #     return redirect("/show_catalog/") # 返回預覽畫面
-        
+
         return redirect("/show_catalog/")
 
 
 def createCourse(request):
     if request.method == 'POST':
-        
+
         name = request.POST["name"].strip()
         descript = request.POST["descript"]
         courseName = request.GET.get("p")
         print(courseName)
-        course_catalog = CourseCatalog.objects.get(CourseCatalogName=courseName)
-        course = TeachCourse.objects.create(course_catalog=course_catalog,TeachCourseName=name,teach_description=descript)
+        course_catalog = CourseCatalog.objects.get(
+            CourseCatalogName=courseName)
+        course = TeachCourse.objects.create(
+            course_catalog=course_catalog, TeachCourseName=name, teach_description=descript)
         course.save()
         # if course:
         #     return redirect("/show_course/") # 返回預覽畫面
@@ -66,41 +66,49 @@ def createCourse(request):
 
 
 # 進入編輯unit網頁儲存資料庫和html file
-def editUnit(request,courseName=None):
-    if request.method == "POST" and courseName: # 如果是表單傳來的資料
-        upLoadForm = EditCourseForm(request.POST,request.FILES) # use form.py產生 form 
-        course = TeachCourse.objects.get(TeachCourseName = courseName)
-       
+def editUnit(request, courseName=None):
+    
+    if request.method == "POST" and courseName:  # 如果是表單傳來的資料
+        upLoadForm = EditCourseForm(
+            request.POST, request.FILES)  # use form.py產生 form
+        course = TeachCourse.objects.get(TeachCourseName=courseName)
+
         if upLoadForm.is_valid():
             upLoadfile = save_htmlFile(request.FILES['file'])
             name = request.POST["name"].strip()
             discript = request.POST["descript"]
-            file_id = hashEncoding(request.FILES['file'].name)
-            unintOfinstance = TeachCourseUnit.objects.create(teach_course=course,unitName=name, unit_description=discript,fileId= file_id)
+            filename = request.FILES['file'].name.split(".")[0]
+            file_id = hashEncoding(filename)
+            
+            unintOfinstance = TeachCourseUnit.objects.create(
+            teach_course=course, unitName=name, unit_description=discript, fileId=file_id)
             unintOfinstance.save()
-            return redirect("/show_unit/")
-        
+            return redirect("/editunit/"+courseName)
+            
     else:
-        # 不是表單傳來的產生表單
-        form  = EditCourseForm(request.POST)
-        allunit = TeachCourseUnit.objects.all() # 沒辦法取得c++所屬的課程
+        # 不是表單傳來的post就產生表單
+        form = EditCourseForm(request.POST)
+        course = TeachCourse.objects.get(TeachCourseName=courseName)
+        allunit = course.teachcourseunit_set.all() # 注意此寫法
 
-    return render(request, "showUnit.html",{"form": form,"courseName" :courseName,"allObject":allunit})
+
+    return render(request, "showUnit.html", {"form": form, "courseName": courseName, "allObject": allunit})
 
 
 def save_htmlFile(f):
     filename = f.name
-    filename = filename.split(".")[0] #remove file extention
-    
-    target = os.path.join(settings.BASE_DIR,"webApp","media", hashEncoding(filename) +'.html')
-    with open(target,'wb') as destination:
+    filename = filename.split(".")[0]  # remove file extention
+
+    target = os.path.join(settings.BASE_DIR, "webApp",
+                          "media", hashEncoding(filename) + '.html')
+    with open(target, 'wb') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
-        
+
     return os.path.basename(target)
 
 
-def hashEncoding(filename,length=15):
+def hashEncoding(filename, length=15):
     nameut8 = filename.encode('utf-8')
     hashValue = sha256(nameut8).hexdigest()
     return hashValue[:length]
