191220688357bf2f64e3f39e5bf7a96ecfb89475
frank <gg@gmail.com>
2023-03-22 14:26:40 +0800

    create  quiz model

--- webApp/admin.py
+++ webApp/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import CourseCatalog, TeachCourse, TeachCourseUnit
+from .models import CourseCatalog, TeachCourse, TeachCourseUnit, Quiz,Question,Choice
 # Register your models here.
 admin.site.register(CourseCatalog)
 admin.site.register(TeachCourse)
 admin.site.register(TeachCourseUnit)
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Choice)
--- webApp/models.py
+++ webApp/models.py
@@ -6,27 +6,51 @@ class CourseCatalog(models.Model):
     CourseCatalogName = models.CharField(max_length=200)
     description = models.TextField(default="#", max_length=200)
     catalogOfpic = models.CharField(default="#", max_length=200)
+
     def __str__(self):
         return self.CourseCatalogName
-    
+
 
 class TeachCourse(models.Model):
     course_catalog = models.ForeignKey(CourseCatalog, on_delete=models.CASCADE)
     TeachCourseName = models.CharField(max_length=200)
     teach_description = models.TextField(max_length=200)
     teachOfpic = models.CharField(default='#', max_length=200)
-    
 
     def __str__(self):
         return self.TeachCourseName
-    
+
 
 class TeachCourseUnit(models.Model):
     teach_course = models.ForeignKey(TeachCourse, on_delete=models.CASCADE)
     unitName = models.CharField(max_length=200)
-    unit_description = models.TextField(max_length=200,null=True)
-    fileId = models.CharField(max_length=200,null=True)
+    unit_description = models.TextField(max_length=200, null=True)
+    fileId = models.CharField(max_length=200, null=True)
 
-    
     def __str__(self):
         return self.unitName
+
+# 測驗
+class Quiz(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+# 題目
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text
+
+#選項
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
